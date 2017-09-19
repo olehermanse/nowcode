c4993c86d94abc4b6d08fe78f7b19f49dd1dd546
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-09-20 01:53:44 +0200

    Allow mac builds to fail in travis
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,6 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update ;
     brew install python3 ;
-    brew install pandoc ;
     brew tap Homebrew/python ;
     pip3 install -U pip wheel ;
     pip3 install -r requirements.txt ;

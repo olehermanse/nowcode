04ed409a82b563bbec6a00150b97b01d1c397900
Michael Pande <michaelpande@gmail.com>
2017-10-01 19:57:16 +0200

    Browserify, uglify, es6

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    del = require('del');
+    del = require('del'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util'),
+    babelify = require('babelify');
 
 gulp.task('default', function() {
   gulp.start('styles', 'scripts', 'html');
@@ -18,9 +25,26 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts'));
+
+  var b = browserify({
+    entries: 'src/scripts/main.js',
+    debug: true
+  });
+
+  return b
+    .transform('babelify', {presets: ["env"]})
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadmaps: true}))
+      .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/scripts'));
+
+  //return gulp.src('src/scripts/**/*.js')
+  //  .pipe(concat('main.js'))
+  //  .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('html', function() {

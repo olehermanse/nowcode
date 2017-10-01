0eacfecbb6948d4d43498ba7fb3cfc2574fc8df6
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-10-01 23:19:22 +0200

    Ensure pip3 requirements are installed on deploy
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- deploy_nowcode.sh
+++ deploy_nowcode.sh
@@ -2,9 +2,12 @@
 
 killall python3
 cd /root/nowcode/ || exit 1
+rm -f nohup.out
 git reset --hard HEAD
 git checkout master
+git reset --hard HEAD
 git pull
-cp /root/nowcode/nowcode.sh /etc/cron.hourly/nowcode.sh
-chmod 700 /etc/cron.hourly/nowcode.sh
+pip3 install -r requirements.txt
+cp /root/nowcode/deploy_nowcode.sh /etc/cron.hourly/deploy_nowcode.sh
+chmod 700 /etc/cron.hourly/deploy_nowcode.sh
 nohup python3 /root/nowcode/server.py --release --port 80 --ip 0.0.0.0 &

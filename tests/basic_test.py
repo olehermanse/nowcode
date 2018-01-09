662ebd64750e03710ff02a7aabc8a9cc2072ce69
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-01-09 23:02:42 +0100

    Adapt tests to new package name
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-if os.path.exists("./server.py"):
+if os.path.exists("./nowcode_server/__init__.py"):
     sys.path.insert(0, "./")
-elif os.path.exists("../server.py"):
+elif os.path.exists("../nowcode_server/__init__.py"):
     sys.path.insert(0, "../")
 
-import server
+import nowcode_server
 
 def test_import():
     """Passes if the import above succeeded"""

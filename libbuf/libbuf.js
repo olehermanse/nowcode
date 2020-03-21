e19d2853d1199ab8e2a41f0134c7ab2c68a9ed44
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-03-21 20:28:01 +0100

    Added libbuf and tests for it
    This is the first iteration of the algorithm / library which
    will be shared by both frontend and backend. It is very simple
    at the moment, but can be expanded.
    
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

---
+++ libbuf/libbuf.js
@@ -0,0 +1,182 @@
+// This class exists just to pick the correct operation class,
+// when receiving operations (JSON) from remote clients/server
+// it is a class and not a function for consistency, the API matches
+// the other classes; Operation.from({}), Insert.from({}) etc.
+class Operation {
+  static from(data) {
+    switch (data.command) {
+      case "content":
+        return Content.from(data);
+      case "insert":
+        return Insert.from(data);
+      case "delete":
+        return Delete.from(data);
+      default:
+        throw "Unkown operation: " + data.command;
+    }
+  }
+
+  identical(other) {
+    return (
+      this.time === other.time &&
+      this.command === other.command &&
+      this.pos === other.pos &&
+      this.string === other.string
+    );
+  }
+
+  before(other) {
+    return new Date(this.time) < new Date(other.time);
+  }
+
+  after(other) {
+    return new Date(this.time) > new Date(other.time);
+  }
+}
+
+class Insert extends Operation {
+  constructor(string, pos, time = new Date().toISOString()) {
+    super();
+    this.time = time;
+    this.command = "insert";
+    this.string = string;
+    this.pos = pos;
+  }
+
+  static from(data) {
+    return new Insert(data.string, data.pos, data.time);
+  }
+
+  apply(content) {
+    let pos = this.pos;
+    if (pos < 0) {
+      pos = content.length + 1 + pos;
+    }
+    return content.slice(0, pos) + this.string + content.slice(pos);
+  }
+}
+
+class Delete extends Operation {
+  constructor(string, pos, time = new Date().toISOString()) {
+    super();
+    this.string = string;
+    this.pos = pos;
+    this.time = time;
+  }
+
+  static from(data) {
+    return new Delete(data.string, data.pos, data.time);
+  }
+
+  apply(content) {
+    let pos = this.pos;
+    let length = this.string.length;
+    if (pos < 0) {
+      pos = content.length + pos;
+    }
+
+    if (pos + length > content.length) {
+      // No-op: attempted to delete something too long
+      return content;
+    }
+
+    let match_string = content.slice(pos, pos + length);
+
+    if (match_string != this.string) {
+      // No-op: attempted to delete string which is not there
+      return content;
+    }
+
+    return content.slice(0, pos) + content.slice(pos + length);
+  }
+}
+
+class Content extends Operation {
+  constructor(string, time = new Date().toISOString()) {
+    super();
+    this.time = time;
+    this.command = "content";
+    this.string = string;
+  }
+
+  static from(data) {
+    return new Content(data.string, data.time);
+  }
+
+  apply(content) {
+    return this.string;
+  }
+}
+
+class LineBuffer {
+  constructor(name = null, operations = [new Content("")], string = null) {
+    this.name = name;
+    this.operations = operations.map(Operation.from);
+    if (string != null) {
+      this.operations.push(new Content(string));
+    }
+  }
+
+  static from(data) {
+    return new LineBuffer(data.name, data.operations);
+  }
+
+  render() {
+    let content = "";
+    for (let op of this.operations) {
+      content = op.apply(content);
+    }
+    return content;
+  }
+
+  insert(data, pos) {
+    this.operations.push(new Insert(data, pos));
+    return this;
+  }
+
+  delete(data, pos) {
+    this.operations.push(new Delete(data, pos));
+    return this;
+  }
+
+  maybeAddOperation(op) {
+    if (op.command === "content") {
+      return;
+    }
+
+    for (let check of this.operations) {
+      if (op.identical(check)) {
+        return;
+      }
+    }
+
+    const length = this.operations.length;
+
+    // Assume at least one operation in this,
+    // all LineBuffers start with one Content operation
+    console.assert(length > 0);
+
+    let first = this.operations[0].time;
+    if (op.before(first)) {
+      return;
+    }
+
+    let i = this.operations.length - 1;
+
+    while (i > 0 && op.before(this.operations[i])) {
+      --i;
+    }
+
+    this.operations.splice(i + 1, 0, op);
+  }
+
+  merge(buffer) {
+    for (let op of buffer.operations) {
+      this.maybeAddOperation(op);
+    }
+  }
+}
+
+module.exports = {
+  LineBuffer
+};
---
+++ test/test.js
@@ -0,0 +1,138 @@
+const LineBuffer = require("../libbuf/libbuf.js").LineBuffer;
+
+var assert = require("assert");
+describe("LineBuffer", function() {
+  it("Can be rendered after JSON conversion", function() {
+    let buf = new LineBuffer().insert("cd", 0).insert("ab", -3);
+    let string = JSON.stringify(buf);
+    let data = JSON.parse(string);
+    let newBuffer = LineBuffer.from(data);
+    assert.equal(newBuffer.render(), "abcd");
+  });
+  it("Can be merged with another buffer", function() {
+    let start = JSON.stringify(new LineBuffer());
+    let one = new LineBuffer(JSON.parse(start)).insert("cd", 0).insert("ab", 0);
+    assert.equal(one.render(), "abcd");
+    let two = new LineBuffer(JSON.parse(start)).insert("ef", -1);
+    assert.equal(two.render(), "ef");
+    one.merge(two);
+    assert.equal(one.render(), "abcdef");
+  });
+  describe("#render()", function() {
+    it("Renders a new empty buffer", function() {
+      assert.equal(new LineBuffer().render(), "");
+    });
+  });
+  describe("#insert()", function() {
+    it("Allows you to insert a character", function() {
+      assert.equal(new LineBuffer().insert("a", 0).render(), "a");
+    });
+    it("Allows you to insert a word", function() {
+      assert.equal(new LineBuffer().insert("hello", 0).render(), "hello");
+    });
+    it("Allows you to insert multiple times", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("a", 0)
+          .insert("b", 1)
+          .insert("c", 2)
+          .render(),
+        "abc"
+      );
+    });
+    it("Allows you to append using negative index (-1)", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("hello, ", 0)
+          .insert("world", -1)
+          .render(),
+        "hello, world"
+      );
+    });
+    it("Allows you to insert using negative index (-2)", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("ad", 0)
+          .insert("bc", -2)
+          .render(),
+        "abcd"
+      );
+    });
+    it("Allows you to prepend using negative index (-3)", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("cd", 0)
+          .insert("ab", -3)
+          .render(),
+        "abcd"
+      );
+    });
+  });
+  describe("#delete()", function() {
+    it("Allows you to delete a character", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("a", 0)
+          .delete("a", 0)
+          .render(),
+        ""
+      );
+    });
+    it("Allows you to delete a word", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("hello world", 0)
+          .delete("hello ", 0)
+          .render(),
+        "world"
+      );
+    });
+    it("Allows you to delete a multi line buffer", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("hello\nworld\n", 0)
+          .delete("hello\nworld\n", 0)
+          .render(),
+        ""
+      );
+    });
+    it("Allows you to delete a newline", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("hello\nworld", 0)
+          .delete("\n", 5)
+          .render(),
+        "helloworld"
+      );
+    });
+    it("Allows you to delete using negative index (-1)", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("abcd", 0)
+          .delete("d", -1)
+          .delete("c", -1)
+          .render(),
+        "ab"
+      );
+    });
+    it("Is skipped if it doesn't apply", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("aaaa", 0)
+          .delete("A", 0)
+          .delete("b", 0)
+          .render(),
+        "aaaa"
+      );
+    });
+    it("Doesn't delete past end", function() {
+      assert.equal(
+        new LineBuffer()
+          .insert("aaaa", 0)
+          .delete("aa", -1) // Skip - there is only 1 'a' before end
+          .render(),
+        "aaaa"
+      );
+    });
+  });
+});

7ff8361a6fd5dfaae7220d4d4cd3e251236f6adb
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-03-29 04:22:35 +0200

    Backend: Implemented redirect to buffer ID
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,16 @@ const LineBuffer = require("../libbuf/libbuf.js").LineBuffer;
 let buffer = new LineBuffer();
 buffer.insert("hello, world!");
 
+function randomID(){
+  return "" + Math.floor(Math.random() * 10000000);
+}
+
+app.get("/", (req, res) => res.redirect(301, "/" + randomID()));
+
+app.get("/:id/", (req, res) => {
+    res.sendFile('./index.html', { root: "frontend/dist" });
+});
+
 app.use(express.static("frontend/dist"));
 
 app.get("/api/buffers", (req, res) => res.send(buffer));

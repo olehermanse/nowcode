a688a4881e8ce5725798759911c988ab57ad4b60
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-10-17 16:54:17 +0200

    Switched to assert.strictEqual() in tests
    VSCode told me that assert.equal() is deprecated.
    
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- test/test.js
+++ test/test.js
@@ -7,31 +7,31 @@ describe("LineBuffer", function () {
     let string = JSON.stringify(buf);
     let data = JSON.parse(string);
     let newBuffer = LineBuffer.from(data);
-    assert.equal(newBuffer.render(), "abcd");
+    assert.strictEqual(newBuffer.render(), "abcd");
   });
   it("Can be merged with another buffer", function () {
     let start = JSON.stringify(new LineBuffer());
     let one = new LineBuffer(JSON.parse(start)).insert("cd", 0, 0);
-    assert.equal(one.render(), "cd");
+    assert.strictEqual(one.render(), "cd");
     let two = new LineBuffer(JSON.parse(start)).insert("ab", 0, 0);
-    assert.equal(two.render(), "ab");
+    assert.strictEqual(two.render(), "ab");
     one = one.merge(two);
-    assert.equal(one.render(), "abcd");
+    assert.strictEqual(one.render(), "abcd");
   });
   describe("#render()", function () {
     it("Renders a new empty buffer", function () {
-      assert.equal(new LineBuffer().render(), "");
+      assert.strictEqual(new LineBuffer().render(), "");
     });
   });
   describe("#insert()", function () {
     it("Allows you to insert a character", function () {
-      assert.equal(new LineBuffer().insert("a", 0, 0).render(), "a");
+      assert.strictEqual(new LineBuffer().insert("a", 0, 0).render(), "a");
     });
     it("Allows you to insert a word", function () {
-      assert.equal(new LineBuffer().insert("hello", 0, 0).render(), "hello");
+      assert.strictEqual(new LineBuffer().insert("hello", 0, 0).render(), "hello");
     });
     it("Allows you to insert multiple times", function () {
-      assert.equal(
+      assert.strictEqual(
         new LineBuffer().insert("a", 0).insert("b", 0, 1).insert("c", 0, 2).render(),
         "abc"
       );
@@ -39,16 +39,16 @@ describe("LineBuffer", function () {
   });
   describe("#remove()", function () {
     it("Allows you to remove a character", function () {
-      assert.equal(new LineBuffer().insert("a", 0, 0).remove("a", 0, 0).render(), "");
+      assert.strictEqual(new LineBuffer().insert("a", 0, 0).remove("a", 0, 0).render(), "");
     });
     it("Allows you to remove a word", function () {
-      assert.equal(
+      assert.strictEqual(
         new LineBuffer().insert("hello world", 0, 0).remove("hello ", 0, 0).render(),
         "world"
       );
     });
     it("Allows you to remove a multi line buffer", function () {
-      assert.equal(
+      assert.strictEqual(
         new LineBuffer()
           .insert("hello\nworld\n", 0, 0)
           .remove("hello\nworld\n", 0, 0)
@@ -57,7 +57,7 @@ describe("LineBuffer", function () {
       );
     });
     it("Allows you to remove a newline", function () {
-      assert.equal(
+      assert.strictEqual(
         new LineBuffer().insert("\n", 0, 0).remove("\n", 0, 0).render(),
         ""
       );
